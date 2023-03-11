5efa955f91f65121007b5fa14fcb3d3431d774db
mberndt92 <maximilian_berndt@icloud.com>
2023-03-11 16:32:04 +0900

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Maximilian Berndt on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
