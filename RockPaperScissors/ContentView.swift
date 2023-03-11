8eef9ef260e815ecd670e1fbbf2de34c4b4a81f4
mberndt92 <maximilian_berndt@icloud.com>
2023-03-11 17:53:37 +0900

    Change gradient based on whether or not you should win

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -64,7 +64,7 @@ struct ContentView: View {
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: winColor, location: 0.3)
+                .init(color: shouldWin ? winColor : loseColor, location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
             .ignoresSafeArea()
             VStack(spacing: 30) {
